4c510df05d78df91eb7ca49e43689fdbb03bc320
Vassilis Kehayas <vkehayas@gmail.com>
2018-12-17 23:41:30 +0100

    Initial commit

---
+++ src/ProcessRmd.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+
+# Helper function to replace YAML header with TOML
+ReplaceHeader = function(fileName) {
+  fileContents = readLines(fileName)
+  yamlInd = grep("---", fileContents)
+  strippedOfHeader = fileContents[(yamlInd[2] + 1):length(fileContents)]
+  newHeader = readLines("header")
+  fileWithNewHeader = c(newHeader, strippedOfHeader)
+  writeLines(fileWithNewHeader, fileName)
+}
+
+# Store file names of files already present
+contents0 = dir()
+
+# Post-process .md file
+ReplaceHeader("index.md")
+
+# Move new files one level up
+newFiles = contents0[!grepl(c(".Rmd|header|.Rproj|.R"),
+                            contents0,
+                            fixed = FALSE)]
+file.copy(newFiles, "../",
+          overwrite = TRUE)
+file.remove(newFiles)
